996500798c476c8071b6e82f79f56d2bf0c8b8ba
Meera vanathi <119102584+meeravanathi@users.noreply.github.com>
2025-04-17 13:51:17 +0530

    Add files via upload

---
+++ src/models/Transaction.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const TransactionSchema = new mongoose.Schema({
+  amount: Number,
+  date: Date,
+  description: String,
+});
+
+export default mongoose.models.Transaction || mongoose.model('Transaction', TransactionSchema);
